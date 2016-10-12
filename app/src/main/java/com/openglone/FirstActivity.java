730bf7f5c3f34f10b21b3bf9214d0de98d35d0ee
yanjuncq <yanjun@caiqiu.com>
2016-10-12 20:37:01 +0800

    Initial commit

---
+++ app/src/main/java/com/openglone/FirstActivity.java
@@ -0,0 +1,18 @@
+package com.openglone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yanjun on 16/10/12.
+ */
+
+public class FirstActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        
+    }
+}
