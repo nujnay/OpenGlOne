68d91c382925accc8242202d8d90df8500cfcf8a
yanjuncq <yanjun@caiqiu.com>
2016-10-13 17:06:39 +0800

    1013 提交

--- app/src/main/java/com/openglone/FirstActivity.java
+++ app/src/main/java/com/openglone/FirstActivity.java
@@ -1,18 +1,52 @@
 package com.openglone;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by yanjun on 16/10/12.
  */
 
 public class FirstActivity extends Activity {
+
+    private boolean supportsEs2;
+    private GLSurfaceView glSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
 
-        
+        final ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x2000;
+        if (supportsEs2) {
+            glSurfaceView = new GLSurfaceView(this);
+            glSurfaceView.setRenderer(new GLRenderer());
+            setContentView(glSurfaceView);
+        } else {
+            setContentView(R.layout.activity_first);
+            Toast.makeText(this, "当前设备不支持OpenGL ES 2.0!", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (glSurfaceView != null) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (glSurfaceView != null) {
+            glSurfaceView.onResume();
+        }
+    }
+
 }
